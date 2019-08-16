9b7a566057efd203fcb806f94ff0f229dff6bec7
Karen Largent <karenlargent@Karens-MacBook-Pro.local>
2019-08-16 15:32:13 -0400

    adding two blank routes files

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require ("express"); 
 
 var app = express (); 
+
+//routes 
+app.use('/', require('./routes/index')); 
+
 var PORT= process.env.PORT || 5000; 
 
 app.listen(PORT, console.log(`Server started on port ${PORT}.`))
