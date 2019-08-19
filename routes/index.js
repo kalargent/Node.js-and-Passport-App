7b4796a0c71fe54bcfdd94ce00ac53bac56275c8
Karen Largent <karenlargent@Karens-MacBook-Pro.local>
2019-08-19 16:54:14 -0400

    fully functioning log in page complete.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 var express =require ("express"); 
 var router = express.Router(); 
+var { ensureAuthenticated } = require("../config/auth");  
 
+// welcome 
 router.get ("/", (req, res) => res.render("welcome")); 
+
+router.get ("/dashboard", ensureAuthenticated, (req, res) => 
+    res.render("dashboard", {
+        name:req.user.name
+    })); 
 
 module.exports = router; 
